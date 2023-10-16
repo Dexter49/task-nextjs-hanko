0606f0c42ff9304808cb4379895a9602c1f71962
Dexter49 <dexterousguru49@yahoo.com>
2023-10-16 18:43:03 +0100

    So, I'm making progress... I've implemented Hanko Auth and it is displayed properly

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
-  return (
-    <div>
-      <p>Hello, World!</p>
-    </div>
-  );
-}
+"use client";
+import { useEffect} from "react";
+import { register } from "@teamhanko/hanko-elements";
+
+  const hankoApi = "https://09445b79-23a9-493e-b992-ea0b58cafdeb.hanko.io";
+  export default function Login() {
+    useEffect(() => {
+      //
+      register(hankoApi ?? "").catch((error) => {
+        console.log(error);
+      });
+    }, []);
+
+    return (
+      <div className="flex min-h-screen justify-center items-center ">
+        <hanko-auth />
+      </div>
+    );
+  }
